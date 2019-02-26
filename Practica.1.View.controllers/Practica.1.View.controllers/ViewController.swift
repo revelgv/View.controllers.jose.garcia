1ca48af854ba299215398ba1f554e637bd972e9e
revelgv <46965303+revelgv@users.noreply.github.com>
2019-02-26 16:38:02 -0600

    Practica1.jose.garcia
    view.controller

---
+++ Practica.1.View.controllers/Practica.1.View.controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practica.1.View.controllers
+//
+//  Created by Universidad Politecnica de gómez Palacio on 26/02/19.
+//  Copyright © 2019 Universidad Politecnica de gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Practica.1.View.controllers/Practica.1.View.controllersTests/Practica_1_View_controllersTests.swift
@@ -0,0 +1,36 @@
+//
+//  Practica_1_View_controllersTests.swift
+//  Practica.1.View.controllersTests
+//
+//  Created by Universidad Politecnica de gómez Palacio on 26/02/19.
+//  Copyright © 2019 Universidad Politecnica de gómez Palacio. All rights reserved.
+//
+
+import XCTest
+@testable import Practica_1_View_controllers
+
+class Practica_1_View_controllersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
